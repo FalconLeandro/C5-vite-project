7eaa6400a0e874978ddc9377493e2d8954437173
Leandro Falcon <cronosteo1034@gmail.com>
2023-05-25 04:26:11 -0300

    clase4 Componentes y props

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Hijo from './components/Hijo';
+import Padre from './components/Padre';
+
+function App() {
+
+  return (
+    <div className="App">
+      
+
+    </div>
+  )
+}
+
+export default App;
+
